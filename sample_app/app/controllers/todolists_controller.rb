49a4686497745328ad89019b2a992e085b438a67
YutaTsutsumi <223yuta.jp@gmail.com>
2021-06-16 12:12:19 +0000

    robocop

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -1,15 +1,15 @@
 class TodolistsController < ApplicationController
   def new
-    #Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
     @list = List.new
   end
 
   def create
-    #1.データを新規登録するためのインスタンス作成
+    # 1.データを新規登録するためのインスタンス作成
     list = List.new(list_params)
-    #2.データをデータベースに保存するためのsaveメソッド実行
+    # 2.データをデータベースに保存するためのsaveメソッド実行
     list.save
-    #3.詳細画面へリダイレクト
+    # 3.詳細画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
 
@@ -31,14 +31,15 @@ class TodolistsController < ApplicationController
     redirect_to todolist_path(list.id)
   end
 
-  def destroy #データ削除
+  def destroy # データ削除
     list = List.find(params[:id])
     list.destroy
     redirect_to todolists_path
   end
 
   private
-  #ストロングパラメータ
+
+  # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
